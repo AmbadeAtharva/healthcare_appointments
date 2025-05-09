d77f3edbae5237a0ac259b2a72ac11271d30d781
AmbadeAtharva <ambade.atharva@gmail.com>
2025-05-09 07:29:53 -0700

    phase 3 pop ups error

--- frontend/src/components/AppointmentList.js
+++ frontend/src/components/AppointmentList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default function AppointmentList({ refreshFlag }) {
   const [appointments, setAppointments] = useState([]);
